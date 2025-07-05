bf179e25c1a7ff3b86f7a77fe9a12bd325fa7399
Quoc Tuan <tranquocuan310120021@gmail.com>
2025-07-05 14:43:52 +0700

    update version frontend

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-            Student Management APP
+            Student Management APP V2 Date 2025-07-05
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
